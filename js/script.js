278cf29bd3af007d428a7da902adf3688ec137bd
俊介 高橋 <shunsukedakahashi@shunsuke-no-MacBook-Pro-2.local>
2015-03-30 22:36:57 +0900

    js modified

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(function(){
 
+	var divPage = $("div[id^=page-]");	
+	var pageFirst = $("div#page-first");
+	var linkPage = $("a[id^=page-]");
+	
+	divPage.css("display","none");
+	pageFirst.css("display","block");
+
+	linkPage.on("click", function(){
+		var jumpTo = $(this).context.id;
+		divPage.css("display","none");
+		$('div#'+jumpTo).css("display","block");	
+	});
+
 });
